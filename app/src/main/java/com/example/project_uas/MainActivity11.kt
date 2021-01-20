f791176db37aba76cd9fd5d01f99de47bb0b5c61
LuthfilK <62377847+LuthfilK@users.noreply.github.com>
2021-01-21 00:17:09 +0700

    Update MainActivity11.kt

--- app/src/main/java/com/example/project_uas/MainActivity11.kt
+++ app/src/main/java/com/example/project_uas/MainActivity11.kt
@@ -3,8 +3,8 @@ package com.example.project_uas
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.project_uas.MainActivity
-import com.example.project_uas.R
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.example.project_uas.catatan.MainActivity2
 import kotlinx.android.synthetic.main.activity_main11.*
 
@@ -22,7 +22,31 @@ class MainActivity11 : AppCompatActivity() {
             startActivity(go)
         }
 
+        info.setOnClickListener {
+            val go = Intent(this@MainActivity11, MainInfo::class.java)
+            startActivity(go)
+        }
 
+
+        close.setOnClickListener {
+            val builder = AlertDialog.Builder(this, R.style.AlertTheme)
+            builder.setTitle("Exit")
+            builder.setIcon(R.drawable.ic_action_warning)
+
+            builder.setMessage("Are you sure want to exit?")
+            builder.setPositiveButton("Yes"){ dialog, which ->
+                finish()
+            }
+
+            builder.setNegativeButton("No"){ dialog, which ->
+                Toast.makeText(this, "You Clicked Over no Button", Toast.LENGTH_LONG).show()
+            }
+            builder.setNeutralButton("Cancel"){ dialog, which ->
+                Toast.makeText(this, "You clicked over cancel button", Toast.LENGTH_LONG).show()
+            }
+            val dialog: AlertDialog = builder.create()
+            dialog.show()
+        }
 
     }
 }
